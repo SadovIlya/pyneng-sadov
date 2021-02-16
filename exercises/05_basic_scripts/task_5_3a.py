ca7ff5f54fa8221da603905c857517dd0c93514d
Vagrant Default User <vagrant@stretch.localdomain>
2021-02-16 03:53:43 +0000

    2

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -6,7 +6,7 @@
 от выбранного режима, задавались разные вопросы в запросе о номере
 VLANа или списка VLANов:
 * для access: 'Введите номер VLAN:'
-* для trunk: 'Введите разрешенные VLANы:'
+* для trunk: 'Введите разрешенные VLANы:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 То есть эту задачу можно решить без использования условия if и циклов for/while.
@@ -25,3 +25,20 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+interface_mode = input("ведите режим работы интерфейса (access/trunk): ")
+interface_type = input("Введите тип и номер интерфейса: ")
+dicts2 = {
+'trunk':'Введите разрешенные VLANы: ',
+'access': 'Введите номер VLAN:'}
+vlans = input(dicts2[interface_mode])
+
+dicts2 = {
+'trunk':'Введите разрешенные VLANы: ',
+'access': 'Введите номер VLAN:'}
+
+dicts = {
+'trunk':trunk_template,
+'access':access_template
+}
+
+print(('\ninterface ' + interface_type+'\n' + '\n'.join(dicts[interface_mode])).format(vlans))
